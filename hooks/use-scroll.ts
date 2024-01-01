f6a03d4c7a9d7210ad2e125b2378e22d78e87457
Christian Martinez <christian@mixdlabs.com>
2024-01-01 14:16:49 -0700

    Hopefully fixed the use-scroll hook. It is scrolling properly in chrome dev environment. We are only running the function if the user is on a sm or md screen. Testing was done to see the breakpoint of when the scroll actually happens. The breakpoint in pixels is 588. Less than 589 will use the function.

--- hooks/use-scroll.ts
+++ hooks/use-scroll.ts
@@ -4,23 +4,26 @@ export default function useScroll(pathname: string) {
   const ref = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    if (ref.current) {
-      if (pathname === '/qwizzes/front-end') {
-        ref.current.scrollTo({
-          top: 0,
-          left: 0,
-          behavior: 'smooth',
-        })
-      } else if (pathname === '/qwizzes/back-end') {
-        ref.current.scrollTo({
-          top: 0,
-          left: 139.1,
-          behavior: 'smooth',
-        })
-      } else {
-        return
+    function handleScroll() {
+      if (ref.current) {
+        if (pathname === '/qwizzes/front-end') {
+          ref.current.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth',
+          })
+        } else if (pathname === '/qwizzes/back-end') {
+          ref.current.scrollTo({
+            top: 0,
+            left: 557,
+            behavior: 'smooth',
+          })
+        } else {
+          return
+        }
       }
     }
+    screen.width < 589 && handleScroll()
   }, [pathname])
 
   return ref
