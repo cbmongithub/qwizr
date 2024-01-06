f01aa41675da858f1f60d7b0cf200e56fad51048
Christian Martinez <christian@mixdlabs.com>
2024-01-06 16:08:50 -0700

    Added undraw images to not found and no content. Adding scores later

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -5,7 +5,7 @@ import { Qwiz } from '@/components/qwiz'
 
 export default async function QuizPage({ params }: QwizPage) {
   const slug = params.slug.join('/')
-  const ENDPOINT = `${process.env.BASE_URL}/api/${slug}`
+  const ENDPOINT = `${process.env.NEXT_PUBLIC_BASE_URL}/api/${slug}`
   const res = await fetch(ENDPOINT, { method: 'GET' })
 
   if (!res.ok) {
